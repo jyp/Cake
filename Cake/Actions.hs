978dbcbd3d0304f0a795a4f9b78e4b23e8305e69
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2013-06-24 20:41:01 +0200

    warnings

--- Cake/Actions.hs
+++ Cake/Actions.hs
@@ -4,11 +4,9 @@ import Cake.Core
 import Cake.Process
 import System.Directory
 import System.FilePath
-import Control.Applicative
 import Control.Monad (when)
 import Control.Monad.RWS (liftIO)
 import Data.List
-import Data.List.Split
 import Data.Char (isSpace)
 
 copy :: FilePath -> FilePath -> Act()
@@ -25,6 +23,7 @@ touch :: FilePath -> Act ()
 touch x = produce x $ do
   system ["touch",x]
   
+remove :: String -> Act ()
 remove x = system ["rm", "-f", x]
     
 readFile :: FilePath -> Act String
@@ -32,22 +31,25 @@ readFile x = do
   need x
   liftIO $ Prelude.readFile x
   
+dropSpaces :: String -> String
 dropSpaces = reverse . dropWhile isSpace . reverse . dropWhile isSpace 
 
 -- | Depend on the input file and its dependencies, as given by the 1st argument.
+chaseDeps :: (String -> [FilePath]) -> FilePath -> Act ()
 chaseDeps includedFiles i = do
   is <- includedFiles <$> Cake.Actions.readFile i
   mapM_ (chaseDeps includedFiles) is
 
 -- | Depend on the input file and its dependencies. If a file cannot
 -- be found, do not depend on it.
+chaseSoftDeps :: (String -> [FilePath]) -> FilePath -> Act ()
 chaseSoftDeps includedFiles i = do
   e <- liftIO $ doesFileExist i
   when e $ do 
     is <- includedFiles <$> Cake.Actions.readFile i
     mapM_ (chaseDeps includedFiles) is
 
-
+(++?) :: String -> String -> String
 s ++? e = if e `isSuffixOf` s then s else s ++ e
 
 
